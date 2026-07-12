4065549a88d9bf7726b3b207ef89de8c14f3e1ef
ThaboLouisSathekge <tl.sathekge@gmail.com>
2026-07-12 19:05:53 +0000

    Gender and Race Check

---
+++ New Notebook 2026-07-12 20_02_41.sql
@@ -0,0 +1,12 @@
+-- Databricks notebook source
+SELECT *
+FROM bright_tv1.data_1.userprofiles;
+
+....Gender Checks
+SELECT DISTINCT Gender
+FROM bright_tv1.data_1.userprofiles;
+
+SELECT DISTINCT Race
+FROM bright_tv1.data_1.userprofiles;
+    
+      
