45b7ffe73ff413a528e175c2a11cbbbfdf596b5d
ThaboLouisSathekge <tl.sathekge@gmail.com>
2026-07-12 20:21:21 +0000

    Race Check & Replace Bright Tv

--- New Notebook 2026-07-12 20_02_41.sql
+++ New Notebook 2026-07-12 20_02_41.sql
@@ -6,7 +6,18 @@ FROM bright_tv1.data_1.userprofiles;
 SELECT DISTINCT Gender
 FROM bright_tv1.data_1.userprofiles;
 
+....Race Checks
 SELECT DISTINCT Race
 FROM bright_tv1.data_1.userprofiles;
-    
-      
+
+SELECT DISTINCT
+   CASE 
+       WHEN Race = 'other' THEN 'unknown'
+       WHEN Race = 'None' THEN 'unknown'
+   ELSE Race
+   END As Race_clean
+FROM bright_tv1.data_1.userprofiles;
+
+-- COMMAND ----------
+
+
